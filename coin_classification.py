c860210a9806111ef9b622c8dcf7c5cb8751a08e
adamas-oui <63675783+adamas-oui@users.noreply.github.com>
2021-01-07 23:15:28 -0500

    with comments

--- coin_classification.py
+++ coin_classification.py
@@ -1,4 +1,4 @@
-#import libraries 
+#import libraries
 import matplotlib.pyplot as plt
 import numpy as np
 import os
@@ -11,9 +11,9 @@ from tensorflow.keras.models import Sequential
 
 from os import listdir
 from os.path import isfile, join
-#import data 
-#create a list of filenames of images and a list of labels
 
+#import data
+#create a list of filenames of images and a list of labels
 authorities_vals = {
     'Alexander' : 0,
     'Ptolemy' : 1,
@@ -21,40 +21,106 @@ authorities_vals = {
     'Seleucus' : 3
 }
 
-filenames = []
-labels = []
+batch_size = 32
+image_size = 175
 
-def GetData(authority_name):
-    for file in os.listdir('images/' + authority_name):
-        filenames.append('images/' + authority_name + file)
-        labels.append(authorities_vals[authority_name])
+#create datasets to be trained and to be tested
+train_ds = tf.keras.preprocessing.image_dataset_from_directory(
+    'images',
+    validation_split=0.2,
+    subset="training",
+    seed=123,
+    image_size=(image_size, image_size),
+    batch_size=batch_size)
 
-GetData('Alexander')
-GetData('Ptolemy')
-GetData('Antiochus')
-GetData('Seleucus')
-    
-#create a dataset returning slices of 'filenames'
-dataset = tf.data.Dataset.from_tensor_slices((filenames, labels))
-# print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
+val_ds = tf.keras.preprocessing.image_dataset_from_directory(
+    'images',
+    validation_split=0.2,
+    subset="validation",
+    seed=123,
+    image_size=(image_size, image_size),
+    batch_size=batch_size)
 
-#parse every image in the dataset using 'map'
-def ParseFunction(filenames, labels):
-    image_string = tf.read_file(filenames)
-    image_decoded = tf.image.decode_jpeg(image_string, channels = 4)
-    image = tf.cast(image_decoded, tf.float32)
-    return image, labels
-ParseFunction(filenames, labels)
-print(image)
-dataset = dataset.map(ParseFunction)
-dataset = dataset.batch(4)
+class_names = train_ds.class_names
 
-#create iterator and final input tensor
-iterator = dataset.make_one_shot_iterator()
-image, labels = iterator.get_next()
+#configure the dataset for performance
+AUTOTUNE = tf.data.experimental.AUTOTUNE
 
+train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
+val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
 
+#standardize the data
+normalization_layer = layers.experimental.preprocessing.Rescaling(1./255)
 
+normalized_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
+image_batch, labels_batch = next(iter(normalized_ds))
+first_image = image_batch[0]
 
+print(np.min(first_image), np.max(first_image))
 
+num_classes = 4
+
+# no process of data argumentation
+# data_augmentation = keras.Sequential(
+#   [
+#     layers.experimental.preprocessing.RandomFlip("horizontal",
+#                                                  input_shape=(image_size,
+#                                                               image_size,
+#                                                               3)),
+#     layers.experimental.preprocessing.RandomRotation(0.1),
+#     layers.experimental.preprocessing.RandomZoom(0.1),
+#   ]
+# )
+
+#create, compile, train the model 
+model = Sequential([
+  layers.experimental.preprocessing.Rescaling(1./255, input_shape=(image_size, image_size, 3)),
+  layers.Conv2D(16, 3, padding='same', activation='relu'),
+  layers.MaxPooling2D(),
+  layers.Conv2D(32, 3, padding='same', activation='relu'),
+  layers.MaxPooling2D(),
+  layers.Conv2D(64, 3, padding='same', activation='relu'),
+  layers.MaxPooling2D(),
+  layers.Dropout(0.1),   
+  layers.Flatten(),
+  layers.Dense(128, activation='relu'),
+  layers.Dense(num_classes)
+])
+
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+model.summary()
+
+#with different testing, the performance reaches max with epoches of 8
+epochs=8
+history = model.fit(
+  train_ds,
+  validation_data=val_ds,
+  epochs=epochs
+)
+
+#visualize the training results
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs_range = range(epochs)
+
+plt.figure(figsize=(8, 8))
+plt.subplot(1, 2, 1)
+plt.plot(epochs_range, acc, label='Training Accuracy')
+plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.title('Training and Validation Accuracy')
+
+plt.subplot(1, 2, 2)
+plt.plot(epochs_range, loss, label='Training Loss')
+plt.plot(epochs_range, val_loss, label='Validation Loss')
+plt.legend(loc='upper right')
+plt.title('Training and Validation Loss')
+plt.show()
 
